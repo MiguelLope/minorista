785dc252f0050cf5fe9fd55a199c4c6bb0f51b6c
MiguelLope <lopezcandanozamiguelgrupo3@gmail.com>
2024-10-07 14:33:07 -0600

    Correxion final de db.js

--- src/db.js
+++ src/db.js
@@ -2,10 +2,11 @@ import mysql from 'mysql2/promise';
 
 // Configurar la conexión a MySQL
 const db = await mysql.createPool({
-  host: "mysql.railway.internal",
+  host: "junction.proxy.rlwy.net",
   user: "root",
   password: "WHoslXFvuOJkudIiWIHEhaLwSmHQbgHh",
-  database: "railway"
+  database: "railway",
+  port: "37356"
 });
 
 export default db;
